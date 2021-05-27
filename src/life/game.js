3d3dfd0606eb76e204855ff11d2cbe5586e82102
yaldar <yaldar.hapak@gmail.com>
2021-05-27 20:02:41 +0200

    adsfasdf

---
+++ src/life/game.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var WIDTH = 10;
+var HEIGHT = 10;
+var state = new Array(WIDTH).fill(0).map(function (el) { return new Array(HEIGHT).fill(0); });
+console.log(state);
---
+++ src/life/game.ts
@@ -0,0 +1,18 @@
+const WIDTH = 10;
+const HEIGHT = 10;
+
+const state = {
+  rows: new Array(HEIGHT),
+  colomns
+  asd
+}
+interface State {
+  rows: number[],
+  colomns: number[]
+}
+const populateCell = (state: State) => {
+
+}
+console.log(state);
+
+export {}
